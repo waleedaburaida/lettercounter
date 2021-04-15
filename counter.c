5d3581d66c9e6e531bcd7e3045a07dfac25aeae2
aburaidw <70150264+waleedaburaida@users.noreply.github.com>
2021-04-14 23:49:04 -0400

    Conflicts resolved and bugs fixed
    Have resolved the contents and fixed the bugs found in the repository for the bonus mark

--- counter.c
+++ counter.c
@@ -1,25 +1,21 @@
-/*******************************************************************************
-* 
-* Purpose: Assignment #3 solution.
-*
-* Author: Kevin Browne
-*
-*******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+#define BUFFER_SIZE 1024
 #define COUNT_SIZE 26
+int max(int count[]);
+int min(int count[]);
 
 int main()
 {
-  char buffer[1024], input, curchar;
+  char buffer[BUFFER_SIZE], input, curchar;
   int i = 0, count[COUNT_SIZE];
+  int spaces = 0;
   int other = 0;
 
   // request and read in the string from the user
   printf("Enter text for analysis: ");
-  while ( (input = getchar()) != '\n' ) {
+  while ( ((input = getchar()) != '\n') && (i < (BUFFER_SIZE - 1))) {
     buffer[i++] = input;
   }
   buffer[i] = '\0';
@@ -33,48 +29,71 @@ int main()
   // toupper() and then we check if it is in the ASCII range for A-Z (65-90)
   // to determine if we need to increment a count... if it is we increment
   // the right index using 65 as an 'offset'.
-  for (i = 0; i < strlen(buffer); i++) {
+  for (i = 0; i <= strlen(buffer); i++) {
     curchar = toupper(buffer[i]);
     if (curchar >= 65 && curchar <= 90) count[curchar - 65]++;
-    else other++;
+    else {
+        other++;
+        if (curchar == ' ') spaces++;
+    }
   }
 
   // Create the letter analysis table
   printf("\n\nLetter Analysis Complete!");
   printf("\n\nLetter    Occurrences    Percentage\n");
-  printf("*****************************************\n");
+  printf("***************\n");
   for (i = 0; i < COUNT_SIZE; i++) {
     printf("%-10c%-15d%-15.2f\n", i + 65,
                                count[i],
                                (((float) count[i]) / strlen(buffer)) * 100);
   }
-  // Output the number of other characters
-  printf("%-10s%-15d%-15.2f\n","Other",
+  printf("\nTotal spaces: %d\n", spaces);
+  printf("%-10s%-15d%-15.2f\n","O",
                               other,
-                              (((float) count[i]) / strlen(buffer)) * 100);
+                              (((float) other) / strlen(buffer)) * 100);
+
 
   // Find the max and min occuring character in the string, in particular the
   // position in the count array of each character
-  int max, min, max_pos, min_pos;
-  max = min = count[0];
-  min_pos = max_pos = 0;
-  for (i = 0; i < COUNT_SIZE; i++) {
-    if (count[i] < min)
-    {
-      min_pos = i;
-      min = count[i];
-    }
+  int max_pos = max(count);
+  int min_pos = min(count);
+  
+  // Output the max and min occuring character, again using 65 as an offset to
+  // output the character character given ASCII A-Z range from 65-90
+  printf("\nMost frequently occuring character: %c\n", 
+         max_pos + 65);
+  printf("Least frequently occurring character: %c.\n", 
+         min_pos + 65);
+
+  return 0;
+}
+
+// Max num of time occured
+int max(int count[])
+{
+  int max = count[0];
+  int max_pos = 0;
+  for (int i = 0; i < 26; i++) {
     if (count[i] > max)
     {
       max_pos = i;
       max = count[i];
     }
-  }
+  }  
+  return max_pos;
+}
 
-  // Output the max and min occuring letter, again using 65 as an offset to
-  // output the character character given ASCII A-Z range from 65-90
-  printf("\nThe most frequently occurring letter is %c.\n", max_pos + 65);
-  printf("The least frequently occurring letter is %c.\n", min_pos + 65);
-
-  return 0;
+// Min number of time occured
+int min(int count[])
+{
+  int min = count[0];
+  int min_pos = 0;
+  for (int i = 0; i < 26; i++) {
+    if (count[i] < min)
+    {
+      min_pos = i;
+      min = count[i];
+    }
+  }  
+  return min_pos;
 }
